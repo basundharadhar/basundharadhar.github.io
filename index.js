17f1f84ea9c2ea56c40a90de6641401defd07bca
basundharadhar <basundhara.dhar@gmail.com>
2025-03-12 16:45:27 -0600

    Final Upload with JavaScript

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
-const greeting = document.getElementById("greeting");
-greeting.style.textAlign = "center";
-greeting.style.marginTop = "50px";
-const username = window.prompt("What's your name?", "enter here");
-//greeting.innerHTML = "Welcome to my website, " + username + "!";
-window.alert("Welcome to my website " + username + "!");
+const d = new Date();
+document.getElementById("date").innerHTML = d;
+date.style.textAlign = "center";
+// greeting.style.marginTop = "50px";
+// const username = window.prompt("What's your name?", "enter here");
+// //greeting.innerHTML = "Welcome to my website, " + username + "!";
+// window.alert("Welcome to my website " + username + "!");
+
+// To display the date in 2 different colors every second  
+function flash() {
+    var text = document.getElementById('date');
+    text.style.color = (text.style.color == 'red') ? 'green' : 'red';
+    text.style.fontWeight = (text.style.fontWeight == '1000') ? '' : '1000';
+}
+var clr = setInterval(flash, 1000);
+
